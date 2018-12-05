134c79c22c139afa76e47a69a4b3fd0d0e16b499
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-04 19:38:32 -0500

    player choice buttons and score outputs working

--- js/game.js
+++ js/game.js
@@ -1,21 +1,18 @@
-// document.addEventListener('DOMContentLoaded', game);
-const rock       = document.querySelector('#rock');
-const paper      = document.querySelector('#paper');
-const scissors   = document.querySelector('#scissors');
-const score      = [0, 0];
-const weapons    = ['rock', 'paper', 'scissors'];
-const randomNum  = Math.floor(Math.random() * weapons.length);
-const compChoice = weapons[randomNum];
+const rock        = document.querySelector('#rock');
+const paper       = document.querySelector('#paper');
+const scissors    = document.querySelector('#scissors');
+const playerScore = document.querySelector('.player-score');
+const compScore   = document.querySelector('.comp-score');
+const score       = [0, 0];
+const weapons     = ['rock', 'paper', 'scissors'];
+const compChoice  = () => {
+    let choice = weapons[Math.floor(Math.random() * weapons.length)];
+    return choice;
+}
 
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * Math.floor(max));
-// }
-
-// rock.addEventListener('click', playRound('rock', compChoice));
-// paper.addEventListener('click', playRound('paper', compChoice));
-// scissors.addEventListener('click', playRound('scissors', compChoice));
-
-rock.addEventListener('click', playRound);
+rock.addEventListener('click', () => {playRound('rock', compChoice());});
+paper.addEventListener('click', () => {playRound('paper', compChoice());});
+scissors.addEventListener('click', () => {playRound('scissors', compChoice());});
 
 function playRound(playerChoice, compChoice) {                
     switch (playerChoice) {
@@ -23,33 +20,52 @@ function playRound(playerChoice, compChoice) {
             if (compChoice === 'paper') {
                 console.log(`You lose!  ${compChoice} beats ${playerChoice}`);
                 score[1]++;
+                //apply textContent that contains score;
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             } else if (compChoice === 'scissors') {
                 console.log(`You win! ${playerChoice} beats ${compChoice}`);
                 score[0]++;
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             } else {
                 console.log('Tie!');
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             }
             break;
         case 'paper':
             if (compChoice === 'scissors') {
                 console.log(`You lose! ${compChoice} beats ${playerChoice}`);
                 score[1]++;
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             } else if (compChoice === 'rock') {
                 console.log(`You win! ${playerChoice} beats ${compChoice}`);
                 score[0]++;
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             } else {
                 console.log('Tie!');
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             }
             break;
         case 'scissors':
             if (compChoice === 'rock') {
                 console.log(`You lose! ${compChoice} beats ${playerChoice}` );
                 score[1]++;
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             } else if (compChoice === 'paper') {
                 console.log(`You win! ${playerChoice} beats ${compChoice}`);
                 score[0]++;
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             } else {
                 console.log('Tie!');
+                playerScore.textContent = `${score[1]}`;
+                compScore.textContent = `${score[0]}`;
             }
             break;
     }
